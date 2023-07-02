fb25f5cead5145db914591e9b23f8b425f5fa6f3
mmoghal <mmoghal29@gmail.com>
2023-07-02 17:23:48 -0400

    modified:   controllers/userController.js modified:   seeds/thought-seeds.js modified:   seeds/user-seeds.js

--- seeds/thought-seeds.js
+++ seeds/thought-seeds.js
@@ -7,7 +7,14 @@ const thoughts = [
       thoughtText: 'Thought 2',
       username: 'user2',
     },
-  
+    {
+        thoughtText: 'Thought 3',
+        username: 'user3',
+      },
+      {
+        thoughtText: 'Thought 4',
+        username: 'user4',
+      },
   ];
   
   module.exports = thoughts;
--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -7,7 +7,14 @@ const users = [
       username: 'user2',
       email: 'user2@example.com',
     },
-    
+    {
+        username: 'user3',
+        email: 'user3@example.com',
+      },
+      {
+        username: 'user4',
+        email: 'user4@example.com',
+      },
   ];
   
   module.exports = users;
